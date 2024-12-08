ce8ad96ff25e15a699bc35c8ea3f16b064b22f3e
Hadil Hammami <hadilhammami670@gmail.com>
2024-12-08 17:27:26 +0100

    adding register & login interfaces

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using e_commercedotNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_commercedotNet.Controllers
+{
+    // Controllers/AccountController.cs
+    public class AccountController : Controller
+    {
+        // Afficher le formulaire d'inscription
+        public IActionResult Register()
+        {
+            // Assurez-vous d'initialiser le modèle avant de le passer à la vue
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+    
+    
+}
